6c3c6511ffba10290a5a84b0a75f67e81a50bc49
trqyc <qieyanchun@gmail.com>
2023-06-27 18:20:16 +0800

    log response

--- middleware/log.js
+++ middleware/log.js
@@ -3,6 +3,12 @@
 
 const logger = (req, res, next) => {
     console.log(new Date(), '[start]', req.method,  req.url,  'params', req.query, "body", req.body)
+    let send = res.send;
+    res.send = c => {
+        console.log(new Date(), '[end]', res.statusCode, c )
+        res.send = send;
+        return res.send(c);
+    }
     next()
 }
 
